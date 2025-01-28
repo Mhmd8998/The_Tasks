eda611a8e02a32edf4bf80d46752882e80396693
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-28 22:29:56 +0300

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
-
+//port of server 
 const port = process.env.PORT||5000;
+//import mongoose connecting 
+const connect
 
 
+//losten to server on port 8000||5000
 app.listen(port,()=>{
   console.log(`app is start on ${port}`);
 })
