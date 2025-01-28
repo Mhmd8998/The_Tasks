dae22b02b9c8c3c1df6677022d12ee3afcfa1a39
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-29 02:52:55 +0300

    Update Task.js

--- server/src/controller/Task.js
+++ server/src/controller/Task.js
@@ -7,9 +7,9 @@ module.exports={
   }),
   createTask:asyncHandler(async (req,res) => {
     const {title} = req.body;
-    if(!title){
+   /* if(!title){
       return res.status(401).json({message:"title is emptying !"});
-    }
+    }*/
     const task = await TaskModel.create({
       title:title
     })
