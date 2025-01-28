7a5c2914c9998a5230ec922eb89c16d576192ca2
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-28 19:13:39 +0300

    Create Mongodb.js

---
+++ server/src/confige/Mongodb.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const url = process.env.URL_DB;
+
+const conecting = ()=>{
+  mongoose.connect(url).then(() => console.log("connect db"))
+}
+
+
+module.exports = conecting;
