e147b8869a3eb0ee6e9331a465ca8ab9faa5a025
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-29 02:39:31 +0300

    Update Task.js

--- server/src/controller/Task.js
+++ server/src/controller/Task.js
@@ -3,7 +3,7 @@ const asyncHandler = require("express-async-handler");
 
 module.exports={
   homeTask : asyncHandler(async (req,res)=>{
-    res.render("../views/todo");
+    res.render("todo");
   }),
   createTask:asyncHandler(async (req,res) => {
     const {title} = req.body;
