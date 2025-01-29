904cc7153b8973d6e4e0378ae742beacd4dc4e0a
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-29 17:41:36 +0300

    Update Task.js

--- server/src/controller/Task.js
+++ server/src/controller/Task.js
@@ -12,5 +12,15 @@ module.exports = {
             title
         });
         return res.redirect("/");
+    }),
+    uptoTask:asyncHandler(async (req,res)=>{
+        const tasks = await TaskModel.find();
+        const tasksid = req.params.id
+        res.render("todo",{tasks,tasksid});
+    }),
+    updateTask:asyncHandler(async (req,res)=>{
+        const tasksid = req.params.id;
+        const tasks = await TaskModel.findByIdAndUpdate(tasksid,{title:req.body.title});
+        return res.redirect("/");
     })
 };
