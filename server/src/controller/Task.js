3f80d230b9a8c309968ad3fba7a76f83e952e48b
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-29 10:37:30 +0300

    Update Task.js

--- server/src/controller/Task.js
+++ server/src/controller/Task.js
@@ -1,18 +1,15 @@
 const TaskModel = require("../model/Task");
 const asyncHandler = require("express-async-handler");
 
-module.exports={
-  homeTask : asyncHandler(async (req,res)=>{
-    res.render("todo");
-  }),
-  createTask:asyncHandler(async (req,res) => {
-    const {title} = req.body;
-   /* if(!title){
-      return res.status(401).json({message:"title is emptying !"});
-    }*/
-    const task = await TaskModel.create({
-      title
+module.exports = {
+    homeTask: asyncHandler(async (req, res) => {
+        res.render("todo");
+    }),
+    createTask: asyncHandler(async (req, res) => {
+        const { title } = req.body;
+        const task = await TaskModel.create({
+            title
+        });
+        return res.redirect("/");
     })
-    return res.redirect("/");
-  })
-}
+};
