33d8b985c9eac50694aa2e95de1b1396b680079a
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-29 17:46:02 +0300

    Update Task.js

--- server/src/router/Task.js
+++ server/src/router/Task.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
-const { createTask, homeTask } = require("../controller/Task");
+const { createTask, homeTask ,uptoTask , updateTask} = require("../controller/Task");
 
 router.route("/create").post(createTask);
 router.route("/").get(homeTask);
+router.route("/upto").get(uptoTask);
+router.route("/update/:id").put(updateTask);
 
 module.exports = router;
