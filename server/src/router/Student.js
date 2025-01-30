c72d5f33fe5f18eef5522ca2daad7769bcd7c004
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-30 19:31:39 +0300

    Update Student.js

--- server/src/router/Student.js
+++ server/src/router/Student.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
-const {getStudent, create} = require("../controller/Student");
+const {getStudent, create,upto, update} = require("../controller/Student");
 
 router.route("/").get(getStudent);
 router.route("/create").post(create);
+router.route("/upto/:id").get(upto);
+router.route("/update/:id").put(update);
 
 module.exports= router;
