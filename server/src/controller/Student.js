6317387478857797f23938be7392f1c195b39358
Mhmd8998 <alnasrmhmd9988@gmail.com>
2025-01-30 19:20:10 +0300

    Update Student.js

--- server/src/controller/Student.js
+++ server/src/controller/Student.js
@@ -17,6 +17,13 @@ module.exports = {
   upto:asyncHandler(async (req,res)=>{
     const studentid= req.params.id;
     const students = await StudentModel.find();
+    return res.render("upto",{students,studentid});
+  }),
+  update:asyncHandler(async (req,res)=>{
+    const studentid= req.params.id;
+    const {name,age} = req.body
+    await StudentModel.updateOne(studentid,{name,age});
+    res.redirect("/student");
     
   })
 }
